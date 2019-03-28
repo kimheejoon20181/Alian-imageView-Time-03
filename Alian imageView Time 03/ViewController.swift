5729ac449d62bc4f90d775ca883d5caa0524ce1a
D7703_22 <d7703_22@promise.local>
2019-03-28 11:32:32 +0900

    Initial Commit

---
+++ Alian imageView Time 03/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alian imageView Time 03
+//
+//  Created by D7703_22 on 2019. 3. 28..
+//  Copyright © 2019년 fb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
