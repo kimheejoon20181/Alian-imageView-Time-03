442dfd0a92d3725af554dea434e3bfc9d698c759
D7703_22 <d7703_22@promise.local>
2019-03-28 12:47:19 +0900

    scsc

--- Alian imageView Time 03/ViewController.swift
+++ Alian imageView Time 03/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alienImageView: UIImageView!
+    @IBOutlet weak var countLabel: UILabel!
+    var count = 1
+    var myTimer = Timer()  //타이머 객체 생성
+    var isAnimating = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alienImageView.image = UIImage(named: "Image\(count)")
+        countLabel.text = String(count)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func playBtn(_ sender: Any) {
+        print("playBtn")
+        
+        //Timer 작동
+        if isAnimating == true {return}
+        else{ isAnimating = true}
+        
+        myTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
     }
-
-
+    
+    @IBAction func pauseBtn(_ sender: Any) {
+        print("pauseBtn")
+        myTimer.invalidate()
+        isAnimating = false
+    }
+    
+    @IBAction func stopBtn(_ sender: Any) {
+        print("stopBtn")
+        myTimer.invalidate()
+        count = 0
+        alienImageView.image = UIImage(named: "Image\(count)")
+        countLabel.text = String(count)
+        isAnimating = false
+    }
+    
+    @objc func doAnimation() {
+        
+        if count == 5 {count = 0}
+        
+        count+=1
+        alienImageView.image = UIImage(named: "Image\(count)")
+        countLabel.text = String(count)
+    }
+    
+    
 }
-
